e85988f14eec35bd7c3fcba6dcc11e2f53eaca16
Michal Swierczewski <michal.swierczewski@pl.ibm.com>
2016-10-13 12:36:04 +0200

    webapp model

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp',
     'django_extensions',
+    'webapp',
     'personal',
 ]
 
--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^webapp/', include('webapp.urls')),
-    url(r'^$', include('personal.urls')),
+    url(r'^webapp/', include('webapp.urls')),	# webapp as a main page, /$ nothing more than webapp/..
+    url(r'^', include('personal.urls')),        # login/password -> integrate it with ldap (how?)
 ]
