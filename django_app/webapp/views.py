b131a9945a7689811974a5d2f842a1ddceaad285
Michal Swierczewski <michal.swierczewski@pl.ibm.com>
2016-10-25 16:16:23 +0200

    views - in progress

--- django_app/webapp/views.py
+++ django_app/webapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from webapp.models import DbClient
+from webapp.models import DbClient, DbSystem
 import datetime
 
 #render -> we do not need to specify context_instance = RequestContext(request)
@@ -10,8 +10,9 @@ import datetime
 
 def index(request):
     #return render(request, 'webapp/main.html')
-    client_var = DbClient.objects.all()
-    context = {'client': client_var}
+    client_var = DbClient.objects.all()           # all() -> SELECT * FROM
+    system_var = DbSystem.objects.all()
+    context = {'client': client_var, 'system': system_var}
     return render(request, 'webapp/main.html', context)
 
 
@@ -31,3 +32,6 @@ def about(request):
     t = loader.get_template('webapp/about.html')
     c = RequestContext(request)
     return HttpResponse(t.render(c))
+
+# In progress
+# https://www.safaribooksonline.com/library/view/django-web-development/9781787121386/ch06s02.html
