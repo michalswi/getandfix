ecf6ccd6841c6b33a946a09e75d11ef9b9600de1
Michal Swierczewski <michal.swierczewski@pl.ibm.com>
2016-10-25 13:51:33 +0200

    views - in progress

--- django_app/webapp/views.py
+++ django_app/webapp/views.py
@@ -5,22 +5,27 @@ from django.template import RequestContext, loader
 from webapp.models import DbClient
 import datetime
 
+#render -> we do not need to specify context_instance = RequestContext(request)
+#render_to_response() -> we do 
+
 def index(request):
     #return render(request, 'webapp/main.html')
-    test_var = DbClient.objects.get().client_name
-    context = {'client': test_var}
+    client_var = DbClient.objects.all()
+    context = {'client': client_var}
     return render(request, 'webapp/main.html', context)
 
-#def test(request):
-#    return render(request, 'webapp/test.html')
 
 def test(request):
+    #return render(request, 'webapp/test.html')
+
     #now = datetime.datetime.now()
     #context = {'current_date': now}
     #return render(request, 'webapp/test.html', context)        
+    
     test_var = DbClient.objects.get().client_name
     context = {'client': test_var}
     return render(request, 'webapp/test.html', context)
+
 
 def about(request):
     t = loader.get_template('webapp/about.html')
