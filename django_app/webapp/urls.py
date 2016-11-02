5c983570c212720c811bb02fd13737013053028c
michals <michal.swierczewski@pl.ibm.com>
2016-11-02 15:57:24 +0100

    ajax in progress

--- django_app/webapp/urls.py
+++ django_app/webapp/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
+    url(r'^$', views.main, name='main'),
+    url(r'^ajax_main/$', views.ajax_main, name='ajax_main'),
     url(r'about/$', views.about, name='about'),
     url(r'test/$', views.test, name='test'),
 ]
--- django_app/webapp/views.py
+++ django_app/webapp/views.py
@@ -2,18 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from webapp.models import DbClient, DbSystem
+from webapp.models import DbClient, DbSystem, DbServer
+
 import datetime
+import json
 
 #render -> we do not need to specify context_instance = RequestContext(request)
 #render_to_response() -> we do 
 
-def index(request):
+def main(request):
     #return render(request, 'webapp/main.html')
     client_var = DbClient.objects.all()           # all() -> SELECT * FROM
     system_var = DbSystem.objects.all()
-    context = {'client': client_var, 'system': system_var}
+    server_var = DbServer.objects.all()
+    context = {'client': client_var, 'system': system_var, 'server': server_var}  # system: aix/rhel -> not needed to display, only example
     return render(request, 'webapp/main.html', context)
+
+def ajax_main(request):
+    #return render(request, 'webapp/ajax_main.html', json.dumps("var":"ala ma kota"))
+    print "REQUEST--->", request.GET['key']
+
+    return HttpResponse(json.dumps({"var":"ala ma kota"}), content_type="application/json")
 
 
 def test(request):
