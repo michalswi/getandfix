e92330c9be15d5e62fcd6b67438514260164decc
michals <michal.swierczewski@pl.ibm.com>
2016-11-16 08:54:25 +0100

    modified: views.py

--- django_app/webapp/views.py
+++ django_app/webapp/views.py
@@ -7,8 +7,8 @@ from webapp.models import DbClient, DbSystem, DbServer
 import datetime
 import json
 
-#render -> we do not need to specify context_instance = RequestContext(request)
-#render_to_response() -> we do 
+##render -> we do not need to specify context_instance = RequestContext(request)
+##render_to_response() -> we do 
 
 def main(request):
     #return render(request, 'webapp/main.html')
@@ -22,25 +22,26 @@ def main(request):
 
 def ajax_main(request):
 
-    #server_var = DbServer.objects.values("id_client_name_id", "server_name")
     print "REQUEST--->", request.GET['key']
 
+    #server_var = DbServer.objects.values("id_client_name_id", "server_name")
+
     server_var = DbServer.objects.filter(id_client_name_id = int(request.GET['key']))
- 
-    print "----------VAl--------", server_var
-    # <QuerySet [<DbServer: server11>, <DbServer: server12>]>
-    
+    #print server_var
+    #<QuerySet [<DbServer: server11>, <DbServer: server12>]>
+
     server_dic = {}
+
     for s in list(server_var):
         server_dic[s.id] = s.server_name
-    data = json.dumps(server_dic)
+    #data = json.dumps(server_dic)
     print "data----->", data
-    #return HttpResponse(data, content_type="application/json")
-    #return HttpResponse(json.dumps('{"var":"ala ma kota"}'), content_type="application/json")
     
-    # JsonResponse takes dict not json
+    ## JsonResponse takes dict not json
     return JsonResponse(server_dic, content_type="application/json")
     #return JsonResponse({1:{"var":"ala ma kota"}}, content_type="application/json")
+    #return HttpResponse(data, content_type="application/json")
+    #return HttpResponse(json.dumps('{"var":"ala ma kota"}'), content_type="application/json")
 
 def test(request):
     #return render(request, 'webapp/test.html')
