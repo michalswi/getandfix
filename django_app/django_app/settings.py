626c07cc0504bc54dbebd326fee6dd41c1fc5530
michals <michal.swierczewski@pl.ibm.com>
2016-12-15 10:38:08 +0100

    Test

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -137,3 +137,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+#LOGIN_REDIRECT_URL = '../webapp'
+LOGIN_REDIRECT_URL = 'account-redirect'
--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, {'template_name': 'webapp/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.login, name='logout'),
     url(r'^admin/', admin.site.urls),
-    url(r'^webapp/', include('webapp.urls')),	  # webapp as a main page, /$ nothing more than webapp/..
-    url(r'^', include('personal.urls')),        # login/password -> integrate it with ldap (how?)
-    #url(r'^login/', include('webapp.urls')),
+    #url(r'^webapp/', include('webapp.urls')),	  # webapp as a main page, /$ nothing more than webapp/..
+    #url(r'^', include('personal.urls')),        # login/password -> integrate it with ldap (how?)
 ]
