ed33c89c6484857ec30acb3e6bbc1f2cbab1000c
michals <michal.swierczewski@pl.ibm.com>
2017-01-03 16:01:02 +0100

    gunicorn script

--- django_app/start_gun.sh
+++ django_app/start_gun.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
-# Start Gunicorn processes
+tart Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn django_app.wsgi:application \
-    --bind 0.0.0.0:8000 \
+    --bind 127.0.0.1:8080 \
     --workers 3
