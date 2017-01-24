70883ac53ed4d0f2fd6d50287a3206d3247e74e5
michals <michal.swierczewski@pl.ibm.com>
2017-01-24 15:41:56 +0100

    if no ip in db for server then use hostname

--- django_app/webapp/ansible_django/ans_exe.py
+++ django_app/webapp/ansible_django/ans_exe.py
@@ -6,11 +6,19 @@ from webapp.models import DbClient, DbSystem, DbServer, DbCommand, DbLog
 def get_ajax(n):
   n.sort()
   print n
-  n1 = DbClient.objects.get(id=1) 
-  n2 = DbServer.objects.get(id=int(n[2][1])).server_ip
-  n3 = DbCommand.objects.get(id=int(n[1][1]))
+  get_client = DbClient.objects.get(id=1) 
+  get_server_ip = DbServer.objects.get(id=int(n[2][1])).server_ip
+  get_server_host = DbServer.objects.get(id=int(n[2][1])).server_name
+  get_command = DbCommand.objects.get(id=int(n[1][1]))
   # each 'os' is by default run in dir getandfix
   # ansible all -i '<hostname/ip>,' comma is required if 'hosts' file is empty
-  f = os.popen("ansible all -i '{},' -u root -m shell -a '{}'".format(n2, n3))
+
+  # server_ip for vagrant
+  if get_server_ip:  
+    f = os.popen("ansible all -i '{},' -u root -m shell -a '{}'".format(get_server_ip, get_command))
+  # server_host for docker (no IP in DB)
+  else:
+    f = os.popen("ansible all -i '{},' -u root -m shell -a '{}'".format(get_server_host, get_command))
+
   print f
   return f.read()
